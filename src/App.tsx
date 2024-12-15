b360589f7d90bbee69e24de7fa22b3ba44d8a3bd
Abhishek <satheabhishek1@gmail.com>
2024-12-15 13:44:31 +0530

    Complete theme colors concept and theme change concept used

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 function App() {
+  const [theme, setTheme] = useState('');
+  function toggleTheme() {
+    if (theme == '') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      setTheme('dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', '');
+      setTheme('');
+    }
+  }
+  console.log("reloaded and theme is", document.documentElement.getAttribute('data-theme'));
   return (
-    <div
-    className='bg-red-500 p-10'
-    >App</div>
+    <div className='h-screen bg-primary  p-10' >
+      <button className=' text-pink-50 font-bold bg-red-600 hover:bg-pink-900 p-5' onClick={toggleTheme}>Toggle Theme</button>
+      <p className='text-secondary' >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo architecto voluptates id illo praesentium, inventore labore dolores in dignissimos porro similique consectetur sapiente placeat numquam, reiciendis excepturi quaerat dolorem quas.</p>
+    </div>
   )
 }
-
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+   
+    // these are extending the colors here :)
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+        'silver': '#ecebff',
+        'bubble_gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        primary: 'rgb(var(--color-primary))',
+        secondary: 'rgb(var(--color-secondary))',
+      },
+    },
   },
   plugins: [],
 }
